2595411ba52d6e2ee3b3a0d18b12da507c11ee39
John P <70922864+jpierce-coder@users.noreply.github.com>
2023-01-23 19:41:13 -0600

    Create countries_of_the_world Gem
    - Created base file structure for CLI Gem

---
+++ lib/countries_of_the_world_cli.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "countries_of_the_world_cli/version"
+
+module CountriesOfTheWorldCli
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ test/test_helper.rb
@@ -0,0 +1,6 @@
+# frozen_string_literal: true
+
+$LOAD_PATH.unshift File.expand_path("../lib", __dir__)
+require "countries_of_the_world_cli"
+
+require "minitest/autorun"
